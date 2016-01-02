f116703a59057df4f86521e50f04f08f6589d6e3
alonso <hector.martinez.a@gmail.com>
2016-01-02 15:19:08 +0100

    x

--- src/tag_dsl_cqp.sh
+++ src/tag_dsl_cqp.sh
@@ -16,10 +16,13 @@ fi
 
 python dsl2ud.py --infile $infile > $tmpfile.ud
 python extract_features.py --infile "$tmpfile.ud" --labels pos > "$tmpfile.fpos"
-rungsted --initial-model $posmodelname --test $tmpfile.fpos --predictions "$tmpfile.pospred"
+rungsted --initial-model $posmodelname --test "$tmpfile.fpos" --predictions "$tmpfile.pospred"
 python lemmatize_pos_predictions.py --pospredictions "$tmpfile.pospred" --udfile "$tmpfile.ud" > "$tmpfile.poslemma"
 python extract_features.py --infile "$tmpfile.poslemma" --labels supersense > "$tmpfile.fss"
 rungsted --initial-model $sstmodelname --test "$tmpfile.fss" --predictions "$tmpfile.sspred"
 python join_sst_predictions_and_dslcqp.py --infile_sstpred "$tmpfile.sspred" --infile_ud "$tmpfile.ud" --infile_cqp $infile  > "$infile.sstpred.cqp"
 
-rm "$tmpfile.*"
+for suffix in ud fpos pospred poslemma fss sspred
+do
+    rm "$tmpfile.$suffix"
+done
